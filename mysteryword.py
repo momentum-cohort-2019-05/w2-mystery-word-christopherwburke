437b88b179226bff5461a280d63c404e1542fa1f
christopherwburke <christopher.w.burke@gmail.com>
2019-06-07 11:18:37 -0400

    Wrote the welcome message and asked for user input for difficulty level.

---
+++ mysteryword.py
@@ -0,0 +1,2 @@
+diff_level = input('Welcome to Mystery Word! Please choose your difficult level. Type "Easy" for Easy, "Medium" for Medium, or "Hard" for Hard. ')
+print(f"You have chosen {diff_level}. Let's begin.")
