34040b447c783e82ef504806080e4c4c50264434
christopherwburke <christopher.w.burke@gmail.com>
2019-06-10 09:25:49 -0400

    Committing to turn in even tho notclose to being done.

---
+++ mystery_word.py
@@ -0,0 +1,18 @@
+#load words.txt into a list
+
+all_words = open('words.txt', 'r')
+full_word_list = [all_words.rstrip('\n')]
+
+#sort list into three separate lists, based on length of the word
+
+diff_level = input('Welcome to Mystery Word! Please choose your difficult level. Type "Easy" for Easy, "Medium" for Medium, or "Hard" for Hard. ')
+print(f"You have chosen {diff_level}! Let's begin.") # might need a loop here so if they enter something other than Easy/Medium/Hard, it will return them 
+
+if diff_level == easy
+    then r
+
+
+# mystery_word = (needs to randomly select a word from the chosen list)
+guess_count = []g
+wrong_guess_limit = 8
+
--- mysteryword.py
+++
@@ -1,2 +0,0 @@
-diff_level = input('Welcome to Mystery Word! Please choose your difficult level. Type "Easy" for Easy, "Medium" for Medium, or "Hard" for Hard. ')
-print(f"You have chosen {diff_level}. Let's begin.")
